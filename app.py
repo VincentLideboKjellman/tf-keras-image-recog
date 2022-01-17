b1d20902c0599f1c2d0b383c2efc838e46c720ab
Vincent Kjellman <vincent.kjellman@changetheagency.se>
2022-01-17 02:01:08 +0100

    Add routes, prepare for frontend usage

--- app.py
+++ app.py
@@ -39,3 +39,35 @@ def model_predict(img, model):
 
     preds = model.predict(x)
     return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Get the image from post request
+        img = base64_to_pil(request.json)
+
+        # Make prediction
+        preds = model_predict(img, model)
+
+        # Process result
+        pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
+        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+
+        result = str(pred_class[0][0][1])               # Convert to string
+        result = result.replace('_', ' ').capitalize()
+
+        return jsonify(result=result, probability=pred_proba)
+
+    return None
+
+if __name__ == '__main__':
+    # app.run(port=5002, threaded=False)
+
+    # Serve the app with gevent
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
