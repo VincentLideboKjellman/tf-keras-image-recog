b6604b9fed110d1e72c366e3fd1674310826b127
Vincent Kjellman <vincent.kjellman@changetheagency.se>
2022-01-17 01:53:51 +0100

    import packages

---
+++ app.py
@@ -0,0 +1,17 @@
+
+# Flask
+from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# utilites
+import numpy as np
+from util import base64_to_pil
